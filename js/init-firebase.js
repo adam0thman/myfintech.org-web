0db14a382ccb2726b86b465721b8e2df454984e9
Nik M Adam Hafiz <adam0thman@gmail.com>
2019-03-18 20:29:23 +0800

    added contact form

--- js/init-firebase.js
+++ js/init-firebase.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDGsek6bhfTV0UvMQO3ZkfLI27rh5Db0DI",
-    authDomain: "imhere-5e2dc.firebaseapp.com",
-    databaseURL: "https://imhere-5e2dc.firebaseio.com",
-    projectId: "imhere-5e2dc",
-    storageBucket: "imhere-5e2dc.appspot.com",
-    messagingSenderId: "932669852417"
+    apiKey: "AIzaSyByucYkNuP84hVntvB-xnKgEj024hMDIm4",
+    authDomain: "myfintechorg.firebaseapp.com",
+    databaseURL: "https://myfintechorg.firebaseio.com",
+    projectId: "myfintechorg",
+    storageBucket: "myfintechorg.appspot.com",
+    messagingSenderId: "283153497930"
   };
   
   //Initialize main firebase app
@@ -16,9 +16,9 @@
     
   // Variables
   var pageURL = window.location.protocol+"//"+window.location.host;
-  var pageLogin = "/login.html";
-  var pageMain = "/main.html";
-  var pageAdmin = "/admin.html";
+  var pageLogin = "/index.html";
+  var pageMain = "/index.html";
+  var pageAdmin = "/index.html";
   console.log("pageURL is: "+pageURL);
   console.log("pageLogin is: "+pageLogin);
   console.log("pageMain is: "+pageMain);
